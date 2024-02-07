0ca89989c7e0a52fd8e247317814a60d1ad6ac95
paiiiigggeeee <91573998+paiiiigggeeee@users.noreply.github.com>
2024-02-07 10:29:59 -0600

    Add files via upload

---
+++ frmStateMaintenance.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace StateMaintenance
+{
+    partial class frmStateMaintenance
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dgStates = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgStates)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(11, 278);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(78, 25);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(442, 278);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(78, 25);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dgStates
+            // 
+            this.dgStates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStates.Location = new System.Drawing.Point(12, 12);
+            this.dgStates.Name = "dgStates";
+            this.dgStates.RowTemplate.Height = 25;
+            this.dgStates.Size = new System.Drawing.Size(508, 241);
+            this.dgStates.TabIndex = 5;
+            this.dgStates.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgStates_CellContentClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frmStateMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(550, 314);
+            this.Controls.Add(this.dgStates);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmStateMaintenance";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "State Maintenance";
+            this.Load += new System.EventHandler(this.frmStateMaintenance_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgStates)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dgStates;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ frmStateMaintenance.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using StateMaintenance.Models;
+
+namespace StateMaintenance
+{
+    public partial class frmStateMaintenance : Form
+    {
+        public frmStateMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        MMABooksContext context = new MMABooksContext();
+        State selectedState = null;
+
+        private const int ModifyIndex = 6;
+        private const int DeleteIndex = 7;
+
+        // private constants for the index values of the Modify and Delete button columns
+
+        private void frmStateMaintenance_Load(object sender, EventArgs e)
+        {
+            DisplayStates();
+        }
+
+        private void DisplayStates()
+        {
+            // get states and bind grid
+            dgStates.Columns.Clear();
+            // format grid
+            var query = context.States
+                .OrderBy(c => c.StateName)
+                .Select(c => new {c.Customers, c.StateName, c.StateCode })
+                .ToList();
+
+            //bind results of linq query to datagridview 
+            dgStates.DataSource = query;
+
+
+
+            //format grid
+            dgStates.Columns[0].Visible = false;
+
+            //resize
+            dgStates.AutoResizeColumns();
+
+            //set column header style
+            dgStates.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 9, FontStyle.Bold);
+            dgStates.AlternatingRowsDefaultCellStyle.BackColor = Color.Beige;
+
+
+            //add to button columns to the grid
+            //add column for modify button
+            var modifyColumn = new DataGridViewButtonColumn()
+            {
+                UseColumnTextForButtonValue = true,
+                HeaderText = "Modify State",
+                Text = "Modify",
+                Name = "colModify"
+            };
+            dgStates.Columns.Insert(ModifyIndex, modifyColumn);
+
+            //add column for delete button
+
+            var deleteColumn = new DataGridViewButtonColumn()
+            {
+                UseColumnTextForButtonValue = true,
+                HeaderText = "Delete State",
+                Text = "Delete",
+                Name = "colDelete"
+            };
+            dgStates.Columns.Insert(DeleteIndex, deleteColumn);
+        }
+
+
+
+
+
+
+        private void ModifyState()
+        {
+            var modifyForm = new frmAddModify()
+            {
+                AddState = false,
+                State = selectedState
+            };
+            DialogResult result = modifyForm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    selectedState = modifyForm.State;
+                    context.SaveChanges();
+                    DisplayStates();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    HandleConcurrencyError(ex);
+                }
+                catch (DbUpdateException ex)
+                {
+                    HandleDatabaseError(ex);
+                }
+                catch (Exception ex)
+                {
+                    HandleGeneralError(ex);
+                }
+            }
+        }
+
+        private void DeleteState()
+        {
+            DialogResult result =
+                MessageBox.Show($"Delete {selectedState.StateName.Trim()}?",
+                "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    context.States.Remove(selectedState);
+                    context.SaveChanges(true);
+                    DisplayStates();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    HandleConcurrencyError(ex);
+                }
+                catch (DbUpdateException ex)
+                {
+                    HandleDatabaseError(ex);
+                }
+                catch (Exception ex)
+                {
+                    HandleGeneralError(ex);
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var addForm = new frmAddModify
+            {
+                AddState = true
+            };
+            DialogResult result = addForm.ShowDialog();
+            if (result == DialogResult.OK)
+            { 
+                try
+                {
+                    selectedState = addForm.State;
+                    context.States.Add(selectedState);
+                    context.SaveChanges();
+                    DisplayStates();
+                }
+                catch (DbUpdateException ex)
+                {
+                    HandleDatabaseError(ex);
+                }
+                catch (Exception ex)
+                {
+                    HandleGeneralError(ex);
+                }
+            }
+        }
+
+        private void HandleConcurrencyError(DbUpdateConcurrencyException ex)
+        {
+            ex.Entries.Single().Reload();
+            var entityState = context.Entry(selectedState).State;
+            if (entityState == EntityState.Detached)
+            {
+                MessageBox.Show("Another user has deleted that state.",
+                "Concurrency Error");
+            }
+            else
+            {
+                string message = "Another user has updated that state.\n" +
+                "The current database values will be displayed.";
+                MessageBox.Show(message, "Concurrency Error");
+            }
+            DisplayStates();
+        }
+
+        private void HandleDatabaseError(DbUpdateException ex)
+        {
+            string errorMessage = "";
+            var sqlException = (SqlException)ex.InnerException;
+            foreach (SqlError error in sqlException.Errors)
+            {
+                errorMessage += "ERROR CODE: " + error.Number + " " +
+                error.Message + "\n";
+            }
+            MessageBox.Show(errorMessage);
+        }
+
+        private void HandleGeneralError(Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.GetType().ToString());
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgStates_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                int customers = Convert.ToInt32(dgStates.Rows[e.RowIndex].Cells[0].Value.ToString().Trim());
+                selectedState = context.States.Find(customers);
+
+                if(e.ColumnIndex == ModifyIndex)
+                {
+                    ModifyState();
+                }
+                if(e.ColumnIndex == DeleteIndex)
+                {
+                    DeleteState();
+                }
+            }
+        }
+    }
+}
